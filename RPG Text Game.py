cab7732e009a0a99a2660dfa4c0b5b0bb8663075
IvanPedroSG <ivanpedrosampaio@gmail.com>
2023-04-14 11:08:54 -0300

    RPG de Texto v0.1
    Grande projeto que vou fazer para por em prática todos os conhecimentos que eu tiver da linguagem Python quando completar meu curso. Ainda em fase inicial.

---
+++ RPG Text Game.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+print('Voz Misteriosa: Qual... Qual era seu nome mesmo? Isso não importa agora, acorde!')
+nome = str(input('Digite seu nome: '))
+print(f'Voz Misteriosa: Acorde Rápido! {nome}, está tudo pegando fogo!')
+sleep(1.5)
+print('5 A N O S  D E P O I S . . .')
+print(f'Pai: {nome}! Acorde! você já fez aniversário há algum tempo, ja é hora de entrar para o exercito do reino!')
+
