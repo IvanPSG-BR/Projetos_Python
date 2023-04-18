867c5a8f16ceccec3bd585327f11d3ecb136474f
IvanPedroSG <ivanpedrosampaio@gmail.com>
2023-04-17 22:45:56 -0300

    Jogo da Advinhação v1.0.1
    Atualização: Existe agora a possibilidade de escolher a dificuldade do jogo dentre as existentes, tornando o jogo um pouco mais interessante.

--- Jogo da Advinhação.py
+++ Jogo da Advinhação.py
@@ -1,27 +1,65 @@
 from time import sleep
 from random import randint
 
+
+def dificuldade_do_jogo(nulo):
+    pensando = nummaximo = 0
+    niveldadificuldade = int(input('Selecione a sua dificuldade: '))
+
+    while True:
+        if niveldadificuldade >= 5 or niveldadificuldade <= 0:
+            niveldadificuldade = int(input('Número incorreto. Digite uma opção válida: '))
+        else:
+            break
+
+    if niveldadificuldade == 1:
+        pensando, nummaximo = randint(1, 11), 10
+    elif niveldadificuldade == 2:
+        pensando, nummaximo = randint(1, 21), 20
+    elif niveldadificuldade == 3:
+        pensando, nummaximo = randint(1, 51), 50
+    elif niveldadificuldade == 4:
+        pensando, nummaximo = randint(1, 101), 100
+
+    return [pensando, nummaximo, niveldadificuldade]
+
+
 def jogo_da_adivinhacao():
     print(f'Ótimo, então vamos começar!')
 
     sleep(1.0)
-    print('Vou pensar em um número de 1 a 20 e você vai tentar advinhar. Você terá 5 tentativas \nPensado...')
+    print("""Existem quatro dificuldades neste jogo:
+[ 1 ] Fácil (número aleatório de 1 a 10);
+[ 2 ] Médio (número aleatório de 1 a 20);
+[ 3 ] Difícil (número aleatório de 1 a 50);
+[ 4 ] Insano (número aleatório de 1 a 100);""")
+    sleep(1.0)
+
+    dificuldade = dificuldade_do_jogo('')
+
+    sleep(1.0)
+    print(f'Vou pensar em um número de 1 a {dificuldade[1]} e você vai tentar advinhar. Você terá '
+          f'5 tentativas\nPensado...')
     sleep(3.0)
 
-    pensando = randint(1, 21)
     usuario = int(input('Digite o número: '))
-    tentativas = 1
+    tentativas = 4
 
-    while usuario != pensando and tentativas < 5:
-        if usuario < pensando:
+    while usuario != dificuldade[0]:
+        if usuario < dificuldade[0]:
             usuario = int(input('Tente um número mais alto: '))
-        elif usuario > pensando:
+            tentativas -= 1
+        elif usuario > dificuldade[0]:
             usuario = int(input('Tente um número mais baixo: '))
-        else:
-            usuario = int(input('Número inválido. Tente novamente: '))
-        tentativas += 1
-    print(f'Parabéns, você acertou! na {tentativas}ª tentativa!' if tentativas < 5 else 'Que pena, acabaram '
-                                                                                        'suas tentativas...')
+            tentativas -= 1
+
+        if usuario == dificuldade[0] and tentativas >= 1:
+            print(f'Parabéns, você acertou faltando {tentativas}ª tentativas!')
+            break
+        elif tentativas == 0:
+            print('Que pena, você perdeu! Acabou seu número de tentativas')
+            break
+
     return 'Fim do Jogo'
 
 
@@ -37,4 +75,4 @@ while pergunta != 'S' or pergunta != 'N':
         print('Bom, que pena, então tchau!')
         break
     else:
-        pergunta = input('Não entendi. Digite novamente por favor: ').upper()
+        pergunta = input('Não entendi. Digite novamente S para sim ou N para não por favor: ').upper()
